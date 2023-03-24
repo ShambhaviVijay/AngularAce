90fc8dd069bed61de9a40ed780572d95a385ea17
ShambhaviVijay <93869762+ShambhaviVijay@users.noreply.github.com>
2023-03-24 23:49:45 +0530

    Update admin.module.ts

--- my-app/src/app/modules/admin/admin.module.ts
+++ my-app/src/app/modules/admin/admin.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { HomeComponent } from './components/home/home.component';
